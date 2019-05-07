fd621e7c7e8c668bccf0339f9b42361ce01dfa60
Abel-Johnson <734157604@qq.com>
2019-05-07 17:46:23 +0800

    既然有了import()，我们可以选择在需要的时候才加载相应的模块，减少了应用初始化时加载大量暂不需要的模块的压力，这能让我们的应用更高效地运行

--- src/index.js
+++ src/index.js
@@ -1,32 +1,45 @@
-// import _ from 'lodash';
-import printMe from './print.js';
-// import './style.css';
-// import Icon from './icon.jpg';
-// import Data from './data.json'; // Data 变量包含可直接使用的 JSON 解析得到的对象
-
+import _ from 'lodash';
+// 其他引入注释...
 
 function component() {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(function(_) {
-    lement = document.createElement('div');
-    const btn = document.createElement('button');
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+    
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // const myIcon = new Image();
+  // myIcon.src = Icon;
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+  // element.appendChild(myIcon);
 
-    element.appendChild(btn);
+  // console.log(Data);
 
-    return element;
-  }).catch(function() {
-    console.log('An error occurred while loading the component')
-  });
+  btn.innerHTML = 'Click me and check the console!';
+  // btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  btn.onclick = function() {
+    import(/* webpackChunkName: "print" */ './print')
+    .then(function(module) {
+      const printMe = module.default; // 引入模块的默认函数
+
+      printMe();
+    });
+  };
+    
+  return element;
+
+  // 原本的动态引入注释...
 }
-component().then(function(component) {
-  document.body.appendChild(component);
-});
-/* 
-* *
-Tips
-注意上面中的\/* webpackChunkName: "lodash" *\/这段注释，它并不是可有可无的，它能帮助我们结合output.chunkFilename把分离出的模块最终命名为lodash.bundle.js而非[id].bundle.js。
- */
+
+document.body.appendChild(component());
+// var element = component();
+// document.body.appendChild(element);
+
+// 热加载部分注释
+
+// component().then(function(component) {
+//    document.body.appendChild(component);
+//  });
--- src/print.js
+++ src/print.js
@@ -1,4 +1,7 @@
+console.log('The print.js module has loaded! See the network tab in dev tools...');
+
 export default function printMe() {
   // consol.log('I get called from print.js!');
-  console.log('I get called from print.js!');
+  // console.log('I get called from print.js!');
+  console.log('Button Clicked: Here\'s "some text"!');
 }
